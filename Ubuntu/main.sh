d2360dfffd883bae75efba9cd86ca4e71f24b5b7
Ivan <losevskiy.ivan@mail.ru>
2026-02-17 03:10:41 +1000

    first commit

---
+++ Ubuntu/main.sh
@@ -0,0 +1,12 @@
+SCRIPT_DIR=$(dirname "$0")
+USERNAME=$(whoami)
+
+sudo apt-get update
+sudo apt-get install openssh-server
+
+cp "$SCRIPT_DIR/keys/key.pub" ~/.ssh/authorized_keys
+sudo chmod 600 ~/.ssh/authorized_keys
+sudo chmod 700 ~/.ssh
+sudo chown -R "$USERNAME" ~/.ssh
+
+/usr/sbin/sshd
