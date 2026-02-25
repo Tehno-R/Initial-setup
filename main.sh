eb312e01a9adb2fd8156466828ca6e91e7a89026
Ivan <losevskiy.ivan@mail.ru>
2026-02-25 17:51:56 +1000

    add support CentOS and echo if system is not support

--- main.sh
+++ main.sh
@@ -5,6 +5,7 @@ HOSTINFO=$(cat /etc/os-release)
 HOST_ARCH="Arch Linux"
 HOST_UBUNTU="Ubuntu"
 HOST_FEDORA="Fedora Linux"
+HOST_CENTOS="CentOS Stream"
 
 
 if [[ ! -z $(echo "$HOSTINFO" | grep "NAME=\"$HOST_ARCH\"") ]]; then
@@ -16,10 +17,18 @@ elif [[ ! -z $(echo "$HOSTINFO" | grep "NAME=\"$HOST_UBUNTU\"") ]]; then
 elif [[ ! -z $(echo "$HOSTINFO" | grep "NAME=\"$HOST_FEDORA\"") ]]; then
     sudo dnf upgrade -y --refresh
     sudo dnf install -y openssh-server
+elif [[ ! -z $(echo "$HOSTINFO" | grep "NAME=\"$HOST_CENTOS\"") ]]; then
+    sudo yum update -y
+    sudo yum install -y openssh-server
+else
+    echo "This system is not support"
+    return
 fi
 
+if [[ -d "~/.ssh" ]]; then
+    mkdir ~/.ssh
+fi
 
-mkdir ~/.ssh
 cp "$SCRIPT_DIR/keys/key.pub" ~/.ssh/authorized_keys
 sudo chown -R "$USERNAME":"$USERNAME" ~/.ssh
 sudo chmod 700 ~/.ssh
