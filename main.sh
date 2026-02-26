a59d9173b5dbed8aeb0168e8fd2f0aaf7251c49a
Ivan <losevskiy.ivan@mail.ru>
2026-02-27 03:19:27 +1000

    add flag -y in ubuntu case in main

--- main.sh
+++ main.sh
@@ -12,8 +12,8 @@ if [[ ! -z $(echo "$HOSTINFO" | grep "NAME=\"$HOST_ARCH\"") ]]; then
     sudo pacman -Suy --noconfirm
     sudo pacman -S --noconfirm openssh
 elif [[ ! -z $(echo "$HOSTINFO" | grep "NAME=\"$HOST_UBUNTU\"") ]]; then
-    sudo apt-get update
-    sudo apt-get install openssh-server
+    sudo apt-get update -y
+    sudo apt-get install -y openssh-server
 elif [[ ! -z $(echo "$HOSTINFO" | grep "NAME=\"$HOST_FEDORA\"") ]]; then
     sudo dnf upgrade -y --refresh
     sudo dnf install -y openssh-server
